af99e95290bb8b49174566f7661440d12ffbf55c
Joao Moreno <jomo@microsoft.com>
2017-07-12 17:15:19 +0200

    Microphone class

--- client/main.ts
+++ client/main.ts
@@ -1,58 +1,77 @@
-function onAudio(stream: MediaStream) {
-  const audioCtx = new AudioContext();
-  const analyser = audioCtx.createAnalyser();
-  analyser.fftSize = 256;
+import { Event, Emitter } from './util';
 
-  const mic = audioCtx.createMediaStreamSource(stream);
-  mic.connect(analyser);
+class Microphone {
 
-  const bufferLength = analyser.frequencyBinCount;
-  const dataArray = new Float32Array(bufferLength);
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-  const canvasCtx = canvas.getContext('2d');
+  private context: AudioContext;
+  private analyser: AnalyserNode;
+  get frequencyBufferSize(): number { return this.analyser.frequencyBinCount; }
 
-  let canvasSize = { width: 0, height: 0 };
-  function updateCanvasSize() {
-    canvasSize = canvas.getBoundingClientRect();
-    canvas.width = canvasSize.width;
-    canvas.height = canvasSize.height;
+  private _onAudio = new Emitter<void>();
+  get onAudio(): Event<void> { return this._onAudio.event; }
+
+  constructor() {
+    this.context = new AudioContext();
+    this.analyser = this.context.createAnalyser();
+    this.analyser.fftSize = 256;
+
+    navigator.getUserMedia({ audio: true }, stream => this.setup(stream), err => console.error(err));
   }
 
-  updateCanvasSize();
-  window.addEventListener('resize', updateCanvasSize);
+  private setup(stream: MediaStream): void {
+    const mic = this.context.createMediaStreamSource(stream);
+    mic.connect(this.analyser);
+  }
 
-  function draw() {
-    requestAnimationFrame(draw);
-
-    analyser.getFloatFrequencyData(dataArray);
-    canvasCtx.fillStyle = 'rgb(255,255,255)';
-    canvasCtx.fillRect(0, 0, canvasSize.width, canvasSize.height);
-
-    canvasCtx.beginPath();
-    canvasCtx.lineWidth = 5;
-    canvasCtx.strokeStyle = 'rgba(128, 0, 0, 0.1)';
-
-    const barWidth = (canvasSize.width / bufferLength) * 2.5;
-    let x = 0;
-
-    for (var i = 0; i < bufferLength; i++) {
-      const barHeight = (dataArray[i] + 140) * 10;
-      const y = canvasSize.height - barHeight / 2;
-
-      if (i === 0) {
-        canvasCtx.moveTo(x, y);
-      } else {
-        canvasCtx.lineTo(x, y);
-      }
-
-      x += barWidth + 1;
-    }
-
-    canvasCtx.stroke();
-  };
-
-  draw();
+  getFloatFrequencyData(buffer: Float32Array): void {
+    this.analyser.getFloatFrequencyData(buffer);
+  }
 }
 
-navigator.getUserMedia({ audio: true }, onAudio, err => console.error(err));
+const mic = new Microphone();
+const bufferLength = mic.frequencyBufferSize;
+const dataArray = new Float32Array(bufferLength);
 
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const canvasCtx = canvas.getContext('2d');
+
+let canvasSize = { width: 0, height: 0 };
+function updateCanvasSize() {
+  canvasSize = canvas.getBoundingClientRect();
+  canvas.width = canvasSize.width;
+  canvas.height = canvasSize.height;
+}
+
+updateCanvasSize();
+window.addEventListener('resize', updateCanvasSize);
+
+function draw() {
+  requestAnimationFrame(draw);
+
+  mic.getFloatFrequencyData(dataArray);
+  canvasCtx.fillStyle = 'rgb(255,255,255)';
+  canvasCtx.fillRect(0, 0, canvasSize.width, canvasSize.height);
+
+  canvasCtx.beginPath();
+  canvasCtx.lineWidth = 5;
+  canvasCtx.strokeStyle = 'rgba(128, 0, 0, 0.1)';
+
+  const barWidth = (canvasSize.width / bufferLength) * 2.5;
+  let x = 0;
+
+  for (var i = 0; i < bufferLength; i++) {
+    const barHeight = (dataArray[i] + 140) * 10;
+    const y = canvasSize.height - barHeight / 2;
+
+    if (i === 0) {
+      canvasCtx.moveTo(x, y);
+    } else {
+      canvasCtx.lineTo(x, y);
+    }
+
+    x += barWidth + 1;
+  }
+
+  canvasCtx.stroke();
+};
+
+draw();
