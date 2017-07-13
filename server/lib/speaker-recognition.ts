eafcec9fb6e6db2e5e25de93b095ae57bf775048
Michel Kaporin <t-mikapo@microsoft.com>
2017-07-13 09:45:03 +0200

    Added speaker recognition functionality.

---
+++ server/lib/speaker-recognition.ts
@@ -0,0 +1,64 @@
+import * as request from 'request';
+const subscriptionKey = require('../../devenv.json').subscriptionKey;
+
+export async function identifySpeaker(audio: Buffer, identificationProfileIds: string[], operationLocation?: string): Promise<string> {
+    if (!operationLocation) {
+        operationLocation = await getOperationLocation(audio, identificationProfileIds);
+    }
+
+    const operationOptions = {
+        url: operationLocation,
+        method: 'GET',
+        headers: {
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    };
+
+    return new Promise<string>((res, rej) => {
+        request(operationOptions, (error, response, body) => {
+            if (error) {
+                return rej(error);
+            }
+
+            const result = JSON.parse(body);
+            console.log(result);
+            if (result.status === 'notstarted' || result.status === 'running') {
+                return res(identifySpeaker(audio, identificationProfileIds, operationLocation)); // add recursion termination at some point, not to rely on service
+            }
+
+            if (result.status === 'failed' || result.processingResult.identifiedProfileId === '00000000-0000-0000-0000-000000000000') {
+                return rej(`Identification failed or no speaker identified. ${result.message}`);
+            }
+
+            console.log(`${result.processingResult.identifiedProfileId}, probability: ${result.processingResult.confidence}`)
+            return res(result.processingResult.identifiedProfileId);
+        });
+    });
+}
+
+function getOperationLocation(audio: Buffer, identificationProfileIds: string[]): Promise<string> {
+    const options = {
+        url: `https://westus.api.cognitive.microsoft.com/spid/v1.0/identify?identificationProfileIds=${identificationProfileIds.join(',')}`,
+        method: 'POST',
+        headers: {
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        },
+        body: audio
+    };
+
+    return new Promise((res, rej) => {
+        request(options, (error, response, body) => {
+            if (error) {
+                return rej(error);
+            }
+            console.log(body);
+            const operationLocation = response.headers['operation-location'];
+            if (operationLocation) {
+                console.log(operationLocation);
+                return typeof operationLocation === 'string' ? res(operationLocation) : rej('Operation location is not a string.');
+            } else {
+                return rej('Failed to obtain operation-location for a speaker.');
+            }
+        });
+    });
+}
--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,48 @@
 import * as express from 'express';
 import * as ws from 'express-ws';
+import { identifySpeaker } from "./lib/speaker-recognition";
+// Test code
+// import * as fs from 'fs';
+// import * as WebSocket from 'ws';
 
 const app = express();
 ws(app);
 
-// const state = {
-//   speakers: {
-//     michel: 0,
-//     joao: 0
-//   }
-// };
+const state = {
+  "Michel": 0,
+  "Joao": 0
+}; // stored in seconds
+
+const speakers = {
+  "2498991d-c5a9-4143-a345-4b8ba23605ea": "Michel"
+}
 
 app.use(express.static(__dirname + '/..'));
 
 app.ws('/', (ws, req) => {
-  ws.on('message', msg => console.log('web socket: ', msg));
-  console.log('socket', req.testing);
+  ws.on('message', async msg => {
+    try {
+      console.log(Object.keys(speakers));
+      const speakerId = await identifySpeaker(msg, Object.keys(speakers));
+      state[speakers[speakerId]] += 5;
+      ws.send(JSON.stringify(state));
+    } catch (e) {
+      console.error(e);
+    }
+  });
 });
 
 app.listen(8080, () => {
   console.log('Web server running in http://localhost:8080');
 });
+
+// Test code
+// const wsx = new WebSocket('ws://localhost:8080');
+// const audioCut = fs.readFileSync('C:/Users/t-mikapo/Documents/Projects/Standup/audio_cuts/michel_16khz.wav');
+// let i = 0;
+// wsx.on('open', function open() {
+//   if (i == 0) {
+//     wsx.send(audioCut);
+//   }
+//   i++;
+// });
