9069a6d911e122762080a609ac7a93c538eadc81
Joao Moreno <jomo@microsoft.com>
2017-07-13 14:45:43 +0200

    indent

--- server/lib/speaker-recognition.ts
+++ server/lib/speaker-recognition.ts
@@ -1,70 +1,70 @@
 import * as request from 'request';
 let subscriptionKey;
 try {
-    subscriptionKey = require('../../devenv.json').srKey;
+	subscriptionKey = require('../../devenv.json').srKey;
 } catch (e) {
-    throw new Error('Please add `srKey` to devenv.json.');
+	throw new Error('Please add `srKey` to devenv.json.');
 }
 
 export async function identifySpeaker(audio: Buffer, identificationProfileIds: string[], operationLocation?: string, retry: number = 0): Promise<string> {
-    if (retry > 5) {
-        return Promise.reject('Speaker identification timed out.');
-    }
+	if (retry > 5) {
+		return Promise.reject('Speaker identification timed out.');
+	}
 
-    if (!operationLocation) {
-        operationLocation = await getOperationLocation(audio, identificationProfileIds);
-    }
+	if (!operationLocation) {
+		operationLocation = await getOperationLocation(audio, identificationProfileIds);
+	}
 
-    const operationOptions = {
-        url: operationLocation,
-        method: 'GET',
-        headers: {
-            'Ocp-Apim-Subscription-Key': subscriptionKey
-        }
-    };
+	const operationOptions = {
+		url: operationLocation,
+		method: 'GET',
+		headers: {
+			'Ocp-Apim-Subscription-Key': subscriptionKey
+		}
+	};
 
-    return new Promise<string>((res, rej) => {
-        request(operationOptions, (error, response, body) => {
-            if (error) {
-                return rej(error);
-            }
+	return new Promise<string>((res, rej) => {
+		request(operationOptions, (error, response, body) => {
+			if (error) {
+				return rej(error);
+			}
 
-            const result = JSON.parse(body);
-            // console.log(result);
-            if (result.status === 'notstarted' || result.status === 'running') {
-                return res(identifySpeaker(audio, identificationProfileIds, operationLocation, retry)); // add recursion termination at some point, not to rely on service
-            }
+			const result = JSON.parse(body);
+			// console.log(result);
+			if (result.status === 'notstarted' || result.status === 'running') {
+				return res(identifySpeaker(audio, identificationProfileIds, operationLocation, retry)); // add recursion termination at some point, not to rely on service
+			}
 
-            if (result.status === 'failed' || result.processingResult.identifiedProfileId === '00000000-0000-0000-0000-000000000000') {
-                return rej(`Identification failed or no speaker identified. ${result.message}`);
-            }
+			if (result.status === 'failed' || result.processingResult.identifiedProfileId === '00000000-0000-0000-0000-000000000000') {
+				return rej(`Identification failed or no speaker identified. ${result.message}`);
+			}
 
-            return res(result.processingResult.identifiedProfileId);
-        });
-    });
+			return res(result.processingResult.identifiedProfileId);
+		});
+	});
 }
 
 function getOperationLocation(audio: Buffer, identificationProfileIds: string[]): Promise<string> {
-    const options = {
-        url: `https://westus.api.cognitive.microsoft.com/spid/v1.0/identify?identificationProfileIds=${identificationProfileIds.join(',')}&shortAudio=true`,
-        method: 'POST',
-        headers: {
-            'Ocp-Apim-Subscription-Key': subscriptionKey
-        },
-        body: audio
-    };
+	const options = {
+		url: `https://westus.api.cognitive.microsoft.com/spid/v1.0/identify?identificationProfileIds=${identificationProfileIds.join(',')}&shortAudio=true`,
+		method: 'POST',
+		headers: {
+			'Ocp-Apim-Subscription-Key': subscriptionKey
+		},
+		body: audio
+	};
 
-    return new Promise((res, rej) => {
-        request(options, (error, response, body) => {
-            if (error) {
-                return rej(error);
-            }
-            const operationLocation = response.headers['operation-location'];
-            if (operationLocation) {
-                return typeof operationLocation === 'string' ? res(operationLocation) : rej('Operation location is not a string.');
-            } else {
-                return rej('Failed to obtain operation-location for a speaker.');
-            }
-        });
-    });
+	return new Promise((res, rej) => {
+		request(options, (error, response, body) => {
+			if (error) {
+				return rej(error);
+			}
+			const operationLocation = response.headers['operation-location'];
+			if (operationLocation) {
+				return typeof operationLocation === 'string' ? res(operationLocation) : rej('Operation location is not a string.');
+			} else {
+				return rej('Failed to obtain operation-location for a speaker.');
+			}
+		});
+	});
 }
